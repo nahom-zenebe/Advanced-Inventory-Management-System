7aed64e08efd2fb22fe7735637455c0e099046a2
nahom zenebe <nahomzenebe123@gmail.com>
2025-02-05 21:21:08 +0300

    adding stockTranscation and supplier

---
+++ backend/models/StockTranscationmodel.js
@@ -0,0 +1,39 @@
+const { Timestamp } = require('mongodb')
+const mongoose=require('mongoose')
+
+
+
+
+const StockTranscationSchema= new mongoose.Schema({
+   product:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Product'
+
+   },
+   type:{
+    type:String,
+    enum:["Stock-in","Stock-out"],
+    required:true,
+    
+   },
+   quantity:{
+    type:Numbber,
+    required:true
+   },
+   transactionDate:{
+    type:Date,
+    default:Date.now
+   },
+   supplier:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Supplier"
+   },
+
+    Timestamp:true
+
+
+})
+
+const StockTranscation=mongoose.model("User",StockTranscationSchema)
+
+module.exports=StockTranscation
---
+++ backend/models/Suppliermodel.js
@@ -0,0 +1,36 @@
+const { Timestamp } = require('mongodb')
+const mongoose=require('mongoose')
+
+
+
+
+const SupplierSchema= new mongoose.Schema({
+
+    name:{
+        type:String,
+        require:true
+    },
+    contactInfo:{
+        phone:{type:String},
+        email:{type:String},
+        address:{type:String}
+    },
+    productsSupplied:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Prodcut"
+
+    }],
+    createdAt:{
+        type:Date,
+        default:Date.now
+
+    },
+
+    Timestamp:true
+
+
+})
+
+const Supplier=mongoose.model("User",SupplierSchema)
+
+module.exports=Supplier
